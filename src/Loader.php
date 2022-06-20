083e2a54cda7843dbc05aa1b5753b2bd134eb26c
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-20 19:51:33 -0300

    Update Loader.php

--- src/Loader.php
+++ src/Loader.php
@@ -1,1 +1,71 @@
+<?php
 
+namespace FakePlayer;
+
+use FakePlayer\{
+ Eventos\Listener, 
+ Commands\FakeCommand, 
+};
+
+use pocketmine\{
+ Server, 
+ utils\Config,
+ player\Player, 
+ plugin\Plugin,
+ plugin\PluginBase, 
+ utils\TextFormat as C, 
+};
+
+class Loader extends PluginBase {
+ 
+ public $fake = array();
+ 
+ public function onEnable() : void {
+  $this->getListener();
+  $this->getCommands();
+  $this->getConfig()->get("permission.use") ;
+  $this->getLogger()->info(C::GREEN . "Plugin FakePlayer ativado!");
+ }
+ 
+ public function onDisable() : void {
+  $this->getLogger()->info(C::RED . "Plugin FakePlayer desativado!");
+ }
+ 
+ public function getListener() {
+  $manager = $this->getServer()->getPluginManager();
+  $manager->registerEvents(new Listener($this), $this); 
+ }
+ 
+ public function getCommands() {
+  $map = $this->getServer()->getCommandMap();
+		$map->register("fake", new FakeCommand($this));  
+ }
+ 
+ public function iFake($function, $xuid, $nametag = "null"){
+  switch ($function) {
+   case "get":
+    $this->getLogger()->notice("get ({$xuid} => {$this->fake[$xuid]})!");
+    return $this->fake["$xuid"];
+    break;
+   case "set":
+    $this->getLogger()->notice("set ({$nametag})!");
+    $this->fake["$xuid"] = $nametag;
+    break;
+   case "unset":
+    $this->getLogger()->notice("unset ({$xuid})!");
+    unset($this->fake["$xuid"]);
+    break;
+   case "has":
+    $this->getLogger()->notice("check => ({$xuid})!");
+    if(isset($this->fake["$xuid"])){
+     $this->getLogger()->notice("check => ({$xuid}) in true!");
+     return true; 
+    }else{
+     $this->getLogger()->notice("check => ({$xuid}) in false!");
+     return false;
+    }
+    break;
+  }
+ }
+ 
+}
